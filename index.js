81ced6b326efd3f77a6d5f60b44a3dcce4260702
Prashant Baid <prashantvbaid@gmail.com>
2016-06-01 18:50:52 +0530

    basic visualization of models and their relations

--- index.js
+++ index.js
@@ -6,16 +6,77 @@ module.exports = visualize;
 
 
 function visualize(loopbackApp, options) {
-  options = _defaults({}, options, { mountPath: '/visualize' });
-  loopbackApp.use(options.mountPath, createDiagram(loopbackApp, options));
+    options = _defaults({}, options, {
+        mountPath: '/visualize'
+    });
+    loopbackApp.use(options.mountPath, createDiagram(loopbackApp, options));
 }
 
 function createDiagram(app, options) {
+    var models = app.models();
+    var remotes = app.remoteObjects();
+    var level = 0;
+    var x = 0;
+    var propArr = [];
+    var edgeArr = [];
+    var idx = 0;
+    var modIdxObj = {};
+    var finalObj = {};
 
-  return function(req, res, next) {
+    for (var i in models) {
+        modIdxObj[models[i].modelName] = i;
+    }
+
+    models.forEach(function(Model) {
+        var remotes = [];
+        Model.sharedClass.methods().forEach(function(method) {
+            remotes.push(method.name);
+        });
+
+        var modelDefinition = app.models[Model.modelName].definition;
+        if (modelDefinition && modelDefinition.properties) {
+            var propString = Object.keys(modelDefinition.properties).toString().split(",").join('<li>');
+
+            var propObj = {};
+            propObj.id = idx;
+            propObj.name = Model.modelName;
+            propObj.props = propString;
+            propObj.color = '#999999';
+            propObj.level = level;
+            propObj.remotes = remotes.toString().split(',').join('<li>');
+            propArr.push(propObj);
+            x++;
+            if (x == 5) {
+                x = 0;
+                level++;
+            }
+        }
+        //edges
+        if (Model.settings.relations) {
+            var relArr = Object.keys(Model.settings.relations);
+            for (var rel in relArr) {
+                if (Model.settings.relations[relArr[rel]].model) {
+                    var edgeObj = {};
+                    edgeObj.from = modIdxObj[Model.modelName];
+                    edgeObj.to = modIdxObj[Model.settings.relations[relArr[rel]].model];
+                    edgeArr.push(edgeObj);
+                }
+            }
+        }
 
 
+        idx++;
+    });
 
-    res.send(404,{'sample':'sample'});
-  };
+
+    finalObj.nodes = propArr;
+    finalObj.edges = edgeArr;
+
+    return function(req, res, next) {
+
+        var render = app.loopback.template(__dirname + '/template.ejs');
+        var html = render(finalObj);
+
+        res.send(200, html);
+    };
 }
