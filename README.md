0358dad0c12d6654f07b34cfef9553b6276c2a42
Mennu <naveenmeher07@gmail.com>
2016-07-12 17:38:50 +0530

    Initial Checkin

---
+++ README.md
@@ -0,0 +1,41 @@
+# loopback-component-visualizer
+
+[![Build status](https://img.shields.io/travis/yantrashala/loopback-component-visualizer/master.svg)](https://travis-ci.org/yantrashala/loopback-component-visualizer)
+
+[![GitHub issues](https://img.shields.io/github/issues/yantrashala/loopback-component-visualizer.svg)](https://github.com/yantrashala/loopback-component-visualizer/issues)
+
+### Introduction
+
+Visualizing a model is sometimes a difficult task. Its becomes more worse when the data model gets larger,
+trying to understand how models relate to eachother.
+
+loopback-component-visualizer helps you in creating a model diagram with the relation for you loopback application.
+
+### Version
+0.1.0
+
+### Table of contents
+* Installation
+* Usage
+
+#### Installation
+
+```sh
+$ npm install loopback-component-visualizer
+```
+
+#### Usage
+
+Inside your component-config.json, add the loopback-component-visualizer and a '/visualize' api will be mounted to your server.
+
+You can browse @ http://host:port/visualize
+
+```sh
+"loopback-component-visualizer": {
+  "mountPath": "/visualize"
+}
+```
+
+#### Preview
+
+![A Relational Model](https://github.com/yantrashala/loopback-component-visualizer/master/preview.png?raw=true "A Relational Model")
---
+++ test/unit-test.js
@@ -0,0 +1,99 @@
+var loopback = require('loopback');
+var visualize = require('../');
+var request = require('supertest');
+var expect = require('chai').expect;
+
+describe('visualize', function() {
+  describe('with default config', function() {
+    beforeEach(createLoopbackApplication());
+
+    it('should redirect to /visualize/', function(done) {
+      request(this.app)
+        .get('/visualize')
+        .expect(200)
+        .end(done);
+    });
+
+
+  });
+});
+
+
+function createLoopbackApplication(basePath) {
+  return function(done) {
+    var app = this.app = loopback({localRegistry: true, loadBuiltinModels : true});
+
+    var customerProperties = {
+      name: {type: String, required: true}
+    };
+    var customerOptions = {
+      "relations": {
+          "address": {
+            "type": "embedsOne",
+            "model": "Address",
+            "property": "billingAddress",
+            "options": {
+              "validate": true,
+              "forceId": false
+            }
+          }
+      }
+    }
+
+
+
+    var addressProperties = {
+      street: {type: String, required: true},
+      city: {type: String, required: true},
+      state: {type: String, required: true}
+    };
+
+    var Address = loopback.Model.extend('Address', addressProperties);
+
+    app.model(Address);
+
+    var BillingAddress = loopback.Model.extend('BillingAddress', addressProperties);
+
+    app.model(BillingAddress);
+
+    var OfficeAddress = loopback.Model.extend('OfficeAddress', addressProperties);
+
+    app.model(OfficeAddress);
+
+    var bookProperties = {
+      name: {type: String, required: true}
+    };
+    
+    var Book = loopback.Model.extend('Book', bookProperties);
+
+    app.model(Book);
+
+    var Address = loopback.Model.extend('Address', addressProperties);
+
+    app.model(Address);
+
+    var Address = loopback.Model.extend('Address', addressProperties);
+
+    app.model(Address);
+
+    var Customer = loopback.Model.extend('Customer', customerProperties, customerOptions);
+
+    Customer.greet = function(msg, cb) {
+      cb(null, 'Greetings... ' + msg);
+    }
+
+    Customer.remoteMethod(
+        'greet',
+        {
+          accepts: {arg: 'msg', type: 'string'},
+          returns: {arg: 'greeting', type: 'string'}
+        }
+    );
+
+    app.model(Customer);
+
+    visualize(app);
+    app.use(app.get('restApiRoot') || '/', loopback.rest());
+    done();
+  };
+}
