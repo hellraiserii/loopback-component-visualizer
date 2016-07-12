0b7758fb92811ac73e1fdfaa8e663d1cea105901
Mennu <naveenmeher07@gmail.com>
2016-07-12 17:57:54 +0530

    Updated Unit test case

--- index.js
+++ index.js
@@ -12,68 +12,83 @@ function visualize(loopbackApp, options) {
 }
 
 function createDiagram(app, options) {
-    var models = app.models();
-    var remotes = app.remoteObjects();
-    var level = 0;
-    var x = 0;
-    var propArr = [];
-    var edgeArr = [];
-    var idx = 0;
-    var modIdxObj = {};
-    var finalObj = {};
 
-    for (var i in models) {
-        modIdxObj[models[i].modelName] = i;
-    }
+    var finalObj = prepareJson(app);
 
-    models.forEach(function(Model) {
-        var remotes = [];
-        Model.sharedClass.methods().forEach(function(method) {
-            remotes.push(method.name);
-        });
-        var modelDefinition = app.models[Model.modelName].definition;
-        if (modelDefinition && modelDefinition.properties) {
-            var propString = Object.keys(modelDefinition.properties).toString().split(",").join('<li>');
-
-            var propObj = {};
-            propObj.id = idx;
-            propObj.name = Model.modelName;
-            propObj.props = propString;
-            propObj.level = level;
-            propObj.remotes = remotes && remotes.toString().split(',').join('<li>');
-            propArr.push(propObj);
-            x++;
-            if (x == 5) {
-                x = 0;
-                level++;
-            }
-        }
-        //edges
-        if (Model.settings.relations) {
-            var relArr = Object.keys(Model.settings.relations);
-            for (var rel in relArr) {
-                if (Model.settings.relations[relArr[rel]].model) {
-                    var edgeObj = {};
-                    edgeObj.from = modIdxObj[Model.modelName];
-                    edgeObj.to = modIdxObj[Model.settings.relations[relArr[rel]].model];
-                    edgeArr.push(edgeObj);
-                }
-            }
-        }
-
-
-        idx++;
-    });
-
-
-    finalObj.nodes = propArr;
-    finalObj.edges = edgeArr;
-
-    return function(req, res, next) {
-
-        var render = app.loopback.template(__dirname + '/template.ejs');
-        var html = render(finalObj);
-
-        res.send(200, html);
-    };
+    return respond(finalObj, app);
 }
+
+function prepareJson(app) {
+
+  var models = (app && app.models && app.models()) || {};
+  var remotes = (app && app.remoteObjects && app.remoteObjects()) || {};
+  var level = 0;
+  var x = 0;
+  var propArr = [];
+  var edgeArr = [];
+  var idx = 0;
+  var modIdxObj = {};
+  var finalObj = {};
+
+  for (var i in models) {
+      modIdxObj[models[i].modelName] = i;
+  }
+
+  models.length && models.forEach(function(Model) {
+      var remotes = [];
+      Model.sharedClass.methods().forEach(function(method) {
+          remotes.push(method.name);
+      });
+      var modelDefinition = app.models[Model.modelName].definition;
+      if (modelDefinition && modelDefinition.properties) {
+          var propString = Object.keys(modelDefinition.properties).toString().split(",").join('<li>');
+
+          var propObj = {};
+          propObj.id = idx;
+          propObj.name = Model.modelName;
+          propObj.props = propString;
+          propObj.level = level;
+          propObj.remotes = remotes && remotes.toString().split(',').join('<li>');
+          propArr.push(propObj);
+          x++;
+          if (x == 5) {
+              x = 0;
+              level++;
+          }
+      }
+      //edges
+      if (Model.settings.relations) {
+          var relArr = Object.keys(Model.settings.relations);
+          for (var rel in relArr) {
+              if (Model.settings.relations[relArr[rel]].model) {
+                  var edgeObj = {};
+                  edgeObj.from = modIdxObj[Model.modelName];
+                  edgeObj.to = modIdxObj[Model.settings.relations[relArr[rel]].model];
+                  edgeArr.push(edgeObj);
+              }
+          }
+      }
+
+
+      idx++;
+  });
+
+
+  finalObj.nodes = propArr;
+  finalObj.edges = edgeArr;
+
+  return finalObj;
+}
+
+function respond(finalObj, app) {
+
+  return function(req, res, next) {
+      var data = finalObj || {};
+      var render = app.loopback.template(__dirname + '/template.ejs');
+      var html = render(data);
+      res.send(200, html);
+  };
+
+}
+
+module.exports.prepareJson = prepareJson;
