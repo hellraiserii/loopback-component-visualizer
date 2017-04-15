16e9c4d35bac43fb1cb6667f8a416bcd5e430fe8
Martín Cortina <hellraiserii@gmail.com>
2017-04-15 01:21:53 -0300

    Improving extra info on sidebar and edge visualization.

--- index.js
+++ index.js
@@ -20,78 +20,122 @@ function createDiagram(app, options) {
 
 function prepareJson(app) {
 
-  var models = (app && app.models && app.models()) || {};
-  var remotes = (app && app.remoteObjects && app.remoteObjects()) || {};
-  var level = 0;
-  var x = 0;
-  var propArr = [];
-  var edgeArr = [];
-  var idx = 0;
-  var modIdxObj = {};
-  var finalObj = {};
+    var models = (app && app.models && app.models()) || {};
+    var remotes = (app && app.remoteObjects && app.remoteObjects()) || {};
+    var level = 0;
+    var x = 0;
+    var propArr = [];
+    var edgeArr = [];
+    var idx = 0;
+    var modIdxObj = {};
+    var finalObj = {};
 
-  for (var i in models) {
-      modIdxObj[models[i].modelName] = i;
-  }
+    for (var i in models) {
+        modIdxObj[models[i].modelName] = i;
+    }
 
-  models.length && models.forEach(function(Model) {
-      var remotes = [];
-      Model.sharedClass.methods().forEach(function(method) {
-          remotes.push(method.name);
-      });
-      var modelDefinition = app.models[Model.modelName].definition;
-      if (modelDefinition && modelDefinition.properties) {
-          var propString = Object.keys(modelDefinition.properties).toString().split(',').join('<li>');
+    models.length && models.forEach(function (Model) {
+        var remotes = [];
+        Model.sharedClass.methods().forEach(function (method) {
+            remotes.push(method.name);
+        });
+        var modelDefinition = app.models[Model.modelName].definition;
+        if (modelDefinition && modelDefinition.properties) {
 
-          var propObj = {};
-          propObj.id = idx;
-          propObj.label = Model.modelName;
-          propObj.props = '<ul><li>' + Object.keys(modelDefinition.properties).toString().split(',').join('<li>\n').replace('"', '') + '</ul>';
-          propObj.level = level;
-          propObj.remotes = remotes && '<ul><li>' + remotes.toString().split(',').join('<li>\n').replace('"', '') + '</ul>';
-          propObj.title = '<h2>' + propObj.label + '</h2><h3>Props:</h3>\n' + propObj.props + '\n<h3>Remotes:</h3>' + propObj.remotes
-          propArr.push(propObj);
-          x++;
-          if (x == 5) {
-              x = 0;
-              level++;
-          }
-      }
-      //edges
-      if (Model.settings.relations) {
-          var relArr = Object.keys(Model.settings.relations);
-          for (var rel in relArr) {
-              if (Model.settings.relations[relArr[rel]].model) {
-                  var edgeObj = {};
-                  edgeObj.from = modIdxObj[Model.modelName];
-                  edgeObj.to = modIdxObj[Model.settings.relations[relArr[rel]].model];
-                  edgeObj.label = relArr[rel] || '';
-                  edgeObj.title = JSON.stringify(Model.settings.relations[relArr[rel]]);
-                  edgeObj.arrows = 'to';
-                  edgeArr.push(edgeObj);
-              }
-          }
-      }
+            var propObj = {};
+            propObj.id = idx;
+            propObj.label = Model.modelName;
+            propObj.props = '<ul><li>' + Object.keys(modelDefinition.properties).toString().split(',').join('<li>\n').replace('"', '') + '</ul>';
+            propObj.level = level;
+            propObj.remotes = remotes && '<ul><li>' + remotes.toString().split(',').join('<li>\n').replace('"', '') + '</ul>';
+            propArr.push(propObj);
+            x++;
+            if (x == 5) {
+                x = 0;
+                level++;
+            }
+        }
+        //edges
+        if (Model.settings.relations) {
+            var relArr = Object.keys(Model.settings.relations);
+            for (var rel in relArr) {
+
+                var relObj = Model.settings.relations[relArr[rel]];
+                if (relObj.model) {
+                    var edgeObj = {};
+                    edgeObj.from = modIdxObj[Model.modelName];
+                    edgeObj.to = modIdxObj[relObj.model];
+                    edgeObj.label = relArr[rel];
+                    edgeObj.model = relObj.model;
+                    edgeObj.type = relObj.type;
+                    edgeObj.foreignKey = relObj.foreignKey;
+                    edgeObj.options = relObj.options;
+                    edgeObj.through = relObj.through;
+                    edgeObj.title = Model.modelName + " " + relObj.type + " " + relObj.model;
+                    if (relObj.through) edgeObj.title += " (through " + edgeObj.through + ")";
+                    //default length and arrow direction
+                    edgeObj.length = 100;
+                    edgeObj.arrows = 'to';
 
 
-      idx++;
-  });
+                    //show middle circle if relation has a through object
+                    if (relObj.through) {
+                        //extend through relation edge lenght so it embraces the middle node
+                        edgeObj.length = 300;
+
+                        edgeObj.arrows = {
+                            'middle': {
+                                'enabled': true,
+                                'type': 'circle'
+                            },
+                            'to': {
+                                'enabled': true,
+                                'type': 'arrow'
+                            }
+                        };
+                    }
+
+                    if (relObj.type == "belongsTo") {
+                        //represent a belongsTo relation with a dashed line
+                        edgeObj.dashes = true;
+                    }
+                    if (relObj.type == "hasAndBelongsToMany") {
+                        //draw a many to many relation with double arrows
+                        edgeObj.arrows = {
+                            'from': {
+                                'enabled': true,
+                                'type': 'arrow'
+                            },
+                            'to': {
+                                'enabled': true,
+                                'type': 'arrow'
+                            }
+                        };
+                    }
+                    edgeArr.push(edgeObj);
+                }
+            }
+        }
 
 
-  finalObj.nodes = propArr;
-  finalObj.edges = edgeArr;
+        idx++;
+    });
 
-  return finalObj;
+
+    finalObj.nodes = propArr;
+    finalObj.edges = edgeArr;
+
+    return finalObj;
 }
 
 function respond(finalObj, app) {
 
-  return function(req, res, next) {
-      var data = finalObj || {};
-      var render = app.loopback.template(__dirname + '/template.ejs');
-      var html = render(data);
-      res.send(200, html);
-  };
+    return function (req, res, next) {
+        var data = finalObj || {};
+        var render = app.loopback.template(__dirname + '/template.ejs');
+        var html = render(data);
+        res.send(200, html);
+    };
 
 }
 
